1d3c57de826bed180be8b1ed7cbed841ee7be4e3
Jungbin Oh <benoh0906@gmail.com>
2019-07-17 14:01:45 -0700

    server use update

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
 app.listen(3000, () => {
     console.log('listening to port 3000')
 })    
